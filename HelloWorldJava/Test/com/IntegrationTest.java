2c2b3d8bc68e79f557905c3d9a64f21f169323fa
Zbysio Nowak <august.sviatoslav@gmail.com>
2025-07-01 23:15:04 +0200

    Dodano integracyjny test (nowa klasa w testach)

--- HelloWorldJava/Test/com/IntegrationTest.java
+++ HelloWorldJava/Test/com/IntegrationTest.java
@@ -1,4 +1,24 @@
 package com;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
 public class IntegrationTest {
+    @Test
+    void testMainAndCalculatorIntegration() {
+        Calculator calc = new Calculator();
+
+        int a = 10;
+        int b = 5;
+
+        int sum = calc.add(a, b);
+        int diff = calc.subtract(a, b);
+
+        String wynikSuma = "Wynik dodawania: " + sum;
+        String wynikRoznica = "Wynik odejmowania: " + diff;
+
+        assertEquals("Wynik dodawania: 15", wynikSuma);
+        assertEquals("Wynik odejmowania: 5", wynikRoznica);
+    }
 }
