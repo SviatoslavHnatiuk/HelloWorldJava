c37dd9d98c4ad99e26b813685cab7f8b603f5ed3
Zbysio Nowak <august.sviatoslav@gmail.com>
2025-07-04 04:40:50 +0200

    upd

--- HelloWorldJava/src/com/Main.java
+++ HelloWorldJava/src/com/Main.java
@@ -28,7 +28,7 @@ public class Main {
             switch (wybor) {
                 case 1:
                     System.out.println("Hello, world!");
-                    Thread.sleep(5000);
+                    Thread.sleep(1000);
                     wybor = 3;
                     break;
                 case 2:
